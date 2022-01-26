cb003a56b226ef2f5b51325a3785240ed5dbff5e
tian <tianwu@outlook.com>
2022-01-26 16:15:57 +1100

    add part3 exercise3.2 - info page

--- index.js
+++ index.js
@@ -32,6 +32,13 @@ app.get("/api/persons", (req, res) => {
   res.json(persons);
 });
 
+app.get("/info", (req, res) => {
+  let numPersons = persons.length;
+  const date = new Date();
+
+  res.send(`<p>Phonebook has info for ${numPersons} people</p><p>${date}</p>`);
+});
+
 const PORT = 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
