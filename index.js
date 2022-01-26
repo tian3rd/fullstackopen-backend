82b085987dcaa764194ba315e732b86fd6e4954d
tian <tianwu@outlook.com>
2022-01-26 16:41:53 +1100

    add part3 exercise3.4 - delete request & test

--- index.js
+++ index.js
@@ -51,6 +51,14 @@ app.get("/api/persons/:id", (req, res) => {
   }
 });
 
+app.delete("/api/persons/:id", (req, res) => {
+  const id = Number(req.params.id);
+  persons = persons.filter((p) => p.id != id);
+
+  //  status code 204: no content
+  res.status(204).end();
+});
+
 const PORT = 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
