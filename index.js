f7f970470f942bc37b49dd1faaadf88e375e4a9f
tian <tianwu@outlook.com>
2022-01-26 21:04:12 +1100

    fix typo

--- index.js
+++ index.js
@@ -98,7 +98,7 @@ app.post("/api/persons", (req, res) => {
   res.json(person);
 });
 
-const PORT = porcess.env.PORT || 3001;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
