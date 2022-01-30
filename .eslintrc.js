281a3af58c38d4e6b97cbe7e73876e48cb1cdc2b
tian <tianwu@outlook.com>
2022-01-30 21:21:14 +1100

    add lint config

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+  env: {
+    browser: true,
+    commonjs: true,
+    es2021: true,
+  },
+  extends: "eslint:recommended",
+  parserOptions: {
+    ecmaVersion: "latest",
+  },
+  rules: {
+    indent: ["error", 2],
+    eqeqeq: "error",
+    "no-trailing-spaces": "error",
+    "object-curly-spacing": ["error", "always"],
+    "arrow-spacing": ["error", { before: true, after: true }],
+    "no-console": 0,
+  },
+};
